61094e689dba4544d079034ebf647fddb993df2d
Artem Sychov <artem.suchov@gmail.com>
2021-07-29 14:51:20 +0300

    finish ThreeSort

---
+++ ThreeSort.java
@@ -0,0 +1,18 @@
+public class ThreeSort { 
+    public static void main(String[] args) { 
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+
+        // compute stats
+        int min    = Math.min(a, Math.min(b, c));
+        int max    = Math.max(a, Math.max(b, c));
+	// add all 3 and substract max and min
+        int median = a + b + c - min - max;
+
+        // print stats
+        System.out.println("Minimum -> " + min);
+        System.out.println("Median -> " + median);
+        System.out.println("Maximum -> " + max);
+    }
+}
